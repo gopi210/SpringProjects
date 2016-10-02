3978bc9dbe11d0581827dfc5e96870274c8b8259
Gopi Dappili <398714@GENPT.NET>
2016-10-02 10:12:33 -0400

    Added latest code and docs

--- esbportal/src/main/webapp/js/ReplayController.js
+++
@@ -1,7 +0,0 @@
-GenuinePartsesbPortal.myApp.controller("ReplayController",["$scope","$http","myService",
-function($scope,$http,myService) {
-	 $http.get("Sample.html")
-  .then(function(response) {
-      $scope.myWelcome = response.data;
-  });
-}]);
